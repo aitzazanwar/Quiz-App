fc955d65d6edd4ea43119637bdbef08b49072169
aitzaz <aitzaz79@gmail.com>
2023-03-05 13:40:56 +0000

    js and html code updated

---
+++ assets/scripts/index.js
@@ -0,0 +1,18 @@
+//Selecting the list section by class.
+const quizSections = document.querySelectorAll(".quiz-section");
+
+// assigning consts to Start section and start button.
+const startSection = $("#start");
+const startButton = $("#start_button");
+
+//Quiz section 
+const quizSection = $("#quizQuestions");
+const timeRemaining = $("#timeRemaining");
+const question = $("#question");
+const choices =$("#choices");
+
+//End, Form Elements
+const endSection= $("#end");
+const endTitle = $("#endTitle");
+const score = $("score");
+
